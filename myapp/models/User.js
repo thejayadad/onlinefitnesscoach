6cb10ce9a122a0a0473f6161412e33c665b744ae
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-02-09 22:24:48 -0500

    update

---
+++ myapp/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    image: String,
+    stripe_customer_id: String,
+    stripe_subscription_item: String,
+    emailVerified: Date,
+}, {timestamps: true})
+
+export default mongoose?.models?.User || mongoose.model("User", UserSchema)
